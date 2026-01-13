5ebd31fec93ed7bbb44b3c47f6259c35ed8b79b7
PatrickCabigan <jgcabigan@student.apc.edu.ph>
2026-01-13 02:46:42 +0000

    added components

---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Patatas',
+      message: 'I like Patatas'
+    }
+  }
+};
+</script>
+
+<style></style>
