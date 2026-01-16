81d20adf709b08503afaa7c9044bf6c14092eff9
PatrickCabigan <jgcabigan@student.apc.edu.ph>
2026-01-16 10:50:09 +0800

    add profile

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -140,5 +140,8 @@
             </div>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/javascript.js"></script>
 </body>
 </html>
